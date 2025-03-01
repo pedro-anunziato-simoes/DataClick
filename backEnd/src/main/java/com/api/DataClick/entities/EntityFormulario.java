b558aa0f0cc5ff95c569ce38e2e2cfb8efaa1583
Pedro Henrique Da Silva Gomes <pedro.anunziato.simoes@gmail.com>
2025-03-01 10:55:16 -0300

    DC008 - Desenvolver Classes Entity

---
+++ backEnd/src/main/java/com/api/DataClick/entities/EntityFormulario.java
@@ -0,0 +1,39 @@
+package com.API.DataClick.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.util.List;
+
+public class EntityFormulario {
+    @Id
+    private String id;
+    @DBRef
+    private EntityAdministrador administrador;
+    private List<String> campos;
+
+    public EntityFormulario(EntityAdministrador administrador, List<String> campos) {
+        this.administrador = administrador;
+        this.campos = campos;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public EntityAdministrador getAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(EntityAdministrador administrador) {
+        this.administrador = administrador;
+    }
+
+    public List<String> getCampos() {
+        return campos;
+    }
+
+    public void setCampos(List<String> campos) {
+        this.campos = campos;
+    }
+}
