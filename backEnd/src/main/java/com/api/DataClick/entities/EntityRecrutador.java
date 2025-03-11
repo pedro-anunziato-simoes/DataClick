76691c5503e61f7812237e256c698300b49756db
Pedro Henrique Da Silva Gomes <pedro.anunziato.simoes@gmail.com>
2025-03-11 15:52:50 -0300

    DC037 - Adicionar classe abstrata Usuario

--- backEnd/src/main/java/com/api/DataClick/entities/EntityRecrutador.java
+++ backEnd/src/main/java/com/api/DataClick/entities/EntityRecrutador.java
@@ -7,48 +7,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "Recrutadores")
-public class EntityRecrutador {
+public class EntityRecrutador extends Usuario{
 
-    @Id
-    private String id;
-    private String nome;
-    private String email;
-    private String telefone;
     private String token;
 
-    public EntityRecrutador(String nome, String email, String telefone, String token) {
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
+    public EntityRecrutador(String nome, String email, String senha, String telefone, String token) {
+        super(null, nome, email, senha, telefone);
         this.token = token;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
     }
 
     public String getToken() {
